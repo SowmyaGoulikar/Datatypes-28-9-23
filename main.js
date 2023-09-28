a36f667a203b94f7fea6ae289cea0f57bcf4e083
SowmyaGoulikar <146078900+SowmyaGoulikar@users.noreply.github.com>
2023-09-28 16:46:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+function stringdt(){
+   var S="Aventador features are:";
+    if(true){
+        document.getElementById("features").textContent=S;
+        
+        S="a.Power streering.";
+        document.getElementById("feature1").textContent=S;
+        
+        S="b.Automatic Climate change.";
+        document.getElementById("feature2").textContent=S;
+        
+        S="c.Passenger Airbag.";
+    document.getElementById("feature3").textContent=S;
+        S="The Price of Lamborghini Aventador starts form : 5 crore."
+    }
+    document.getElementById("price").textContent=S;
+}
+function booleandt(){
+    let H="Urus features are as follows:";
+    if(true){
+        document.getElementById("positives").textContent=H;
+        
+        H="1.Child Safety locks.";
+        document.getElementById("positives1").textContent=H;
+        
+        H="2.Acceleration of urus is 0-100 km/h in 3.6 seconds.";
+        document.getElementById("positives2").textContent=H;
+        
+        H="3.Airbags.";
+        document.getElementById("positives3").textContent=H;
+        
+        H="Starting price of this Lamborghini Urus model is : 4.28 crore."
+        document.getElementById("price1").textContent=H;
+    }
+}
+function nulldt(){
+    var info="Huracan features are shown below :";
+    if(true){
+        document.getElementById("advantages").textContent=info;
+        
+        info="i) Airbags."
+        document.getElementById("advantages1").textContent=info;
+        
+        info="ii) Brake Assist.";
+        document.getElementById("advantages2").textContent=info;
+        
+        info="iii) Central Locking.";
+        document.getElementById("advantages3").textContent=info;
+        
+        info="The price of Lamborghini Huracan is starting from : 3.12 crore.";
+        document.getElementById("price2").textContent=info;
+        
+    }
+}
+
+function sowmya(){
+    let myArray=["sowmya","Harshini","Sahithya"]
+    Alpa="sowmya";
+    document.getElementById("array").textContent=Alpa;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
